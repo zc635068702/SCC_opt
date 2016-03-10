0edaca525c4bc8d1238f3f1b7bbc9d6a46c1d9cb
Krishna Rapaka <krapaka@apple.com>
2016-03-10 21:58:11 +0000

    SCC : Remove a warning

--- source/Lib/TLibCommon/TComHash.cpp
+++ source/Lib/TLibCommon/TComHash.cpp
@@ -769,8 +769,7 @@ Bool TComHash::getBlockHashValue( const TComPicYuv* const pPicYuv, Int width, In
 
   //4x4 subblock hash values to current block hash values
   for (Int subWidth = 4; subWidth <= width; subWidth *= 2)
-  {
-    Int srcBlockWidth = subWidth >> 1;
+  {   
     srcIdx = 1 - srcIdx;
     dstIdx = 1 - dstIdx;
 
