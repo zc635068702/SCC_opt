8d2cfbe105303a0f036301a43366cd08b38daf5b
Bin Li <libin@microsoft.com>
2018-08-23 10:12:43 +0000

    SCC: change version to 16.18_SCM8.8.

--- source/Lib/TLibCommon/CommonDef.h
+++ source/Lib/TLibCommon/CommonDef.h
@@ -64,7 +64,7 @@ inline Int64 abs (Int64 x) { return _abs64(x); };
 // Version information
 // ====================================================================================================================
 
-#define NV_VERSION        "16.18_SCM8.7"                 ///< Current software version
+#define NV_VERSION        "16.18_SCM8.8"                 ///< Current software version
 
 // ====================================================================================================================
 // Platform information
